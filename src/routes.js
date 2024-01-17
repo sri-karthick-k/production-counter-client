0ad463ab6f9dc61ce45f33517157f67f93e971c5
sahay-aaditya-raj <sahay.aaditya.raj@gmail.com>
2024-01-17 14:54:18 +0530

    main

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
 
 import SignUp from "layouts/authentication/sign-up";
-import Report from "layouts/report";
+
 // @mui icons
 import Icon from "@mui/material/Icon";
 
@@ -26,50 +26,15 @@ const routes = [
   },
   {
     type: "collapse",
-<<<<<<< Updated upstream
-    name: "Billing",
-    key: "billing",
-    icon: <Icon fontSize="small">receipt_long</Icon>,
-    route: "/billing",
-    component: <Billing />,
-  },
-  {
-    type: "collapse",
-    name: "Notifications",
-    key: "notifications",
-    icon: <Icon fontSize="small">notifications</Icon>,
-    route: "/notifications",
-    component: <Notifications />,
-  },
-  {
-    type: "collapse",
-    name: "Profile",
-    key: "profile",
-    icon: <Icon fontSize="small">person</Icon>,
-    route: "/profile",
-    component: <Profile />,
-  },{
-    type: "collapse",
     name: "Report",
     key: "Report",
     icon: <Icon fontSize="small">Report</Icon>,
-    route: "/report",
+    route: "/front/report",
     component: <Report />,
   },
   {
     type: "collapse",
-    name: "Sign In",
-    key: "sign-in",
-    icon: <Icon fontSize="small">login</Icon>,
-    route: "/authentication/sign-in",
-    component: <SignIn />,
-  },
-  {
-    type: "collapse",
-    name: "Sign Up",
-=======
     name: "Create User",
->>>>>>> Stashed changes
     key: "sign-up",
     icon: <Icon fontSize="small">person</Icon>,
     route: "/front/create-user",
