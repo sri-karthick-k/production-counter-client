2be51b319547a9485fcd79fd3c3247de0165a173
Akash Mayya <mayyaakash19@gmail.com>
2024-01-17 14:43:57 +0530

    report3

--- src/layouts/report/data/Values.js
+++ src/layouts/report/data/Values.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-export default function Data() {
+export default function Data(selectedDevice) {
   const [data, setData] = useState({
     columns: [
       { Header: "device Id", accessor: "mac", width: "45%", align: "left" },
@@ -14,13 +14,14 @@ export default function Data() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://192.46.211.177:4001/api/device-report', {
+        const response = await fetch("http://192.46.211.177:4001/api/device-report", {
           headers: {
-            device_id: 'B0:B2:1C:42:BC:9C'
+            device_id: selectedDevice.value,
           },
         });
+
         if (!response.ok) {
-          throw new Error('Failed to fetch data');
+          throw new Error("Failed to fetch data");
         }
 
         const jsonData = await response.json();
@@ -29,12 +30,12 @@ export default function Data() {
           rows: jsonData,
         });
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, []); // Empty dependency array to run the effect only once on component mount
+  }, [selectedDevice]);
 
   return data;
 }
