7db5ea9248047c2ebc10c325adf03e6ceb742866
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2024-04-19 03:41:13 +0530

    add loading animation

--- src/layouts/dashboard/devices.js
+++ src/layouts/dashboard/devices.js
@@ -1,55 +1,65 @@
 import { useState, useEffect } from 'react';
 import config from 'config';
 import DataComponent from './dataComponent';
+import Loader from 'components/Loading/Loading';
 
-const Devices = ()=>{
+const Devices = () => {
 
     const [devices, setDevices] = useState([]);
     const uid = localStorage.getItem("uid");
-    
+    const [loading, setLoading] = useState(true)
+
     // data
-    useEffect(()=>{
-        const getData = async ()=>{
-            try{
+    useEffect(() => {
+        const getData = async () => {
+            try {
                 const response = await fetch(`${config.server.hostname}:${config.server.port}${config.apiKeys.getDevices}`, {
                     method: 'GET',
                     headers: {
-                    'Content-Type': 'application/json',
-                    'user_id': uid
+                        'Content-Type': 'application/json',
+                        'user_id': uid
                     }
                 });
-        
+
                 const data = await response.json();
-        
-                if (response.status===401){
+
+                if (response.status === 401) {
+                    // alert(data['error'])
+                    setLoading(false)
+                }
+                if (response.status === 500) {
                     // alert(data['error'])
                 }
-                if (response.status===500){
-                    // alert(data['error'])
-                }
-                if (response.status===200){
+                if (response.status === 200) {
                     setDevices(data)
-                    }
-            } catch{
+                    setLoading(false)
+                }
+            } catch {
                 alert('FrontEnd Error')
             }
-        
+
         }
         getData()
-    },[uid])
-    
+    }, [uid])
 
 
-    return(
-        <div style={{display:'flex',flexWrap: 'wrap',justifyContent:'space-evenly'}}>
-                {devices.length<=0 ? (<h1>Loading...</h1>):(
-                    <>
-                    {devices.map((val, id)=>{
-                        return <DataComponent data={val} key={id}/>
-                    })}
-                    </>
-                    )}
-                </div>
+
+    return (
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+            {
+                loading
+                    ?
+                    (<Loader />) :
+
+                    devices.length <= 0 ? (<h1>No device found</h1>) : (
+                        <>
+                            {devices.map((val, id) => {
+                                return <DataComponent data={val} key={id} />
+                            })}
+                        </>
+                    )
+            }
+        </div>
     )
 }
 
